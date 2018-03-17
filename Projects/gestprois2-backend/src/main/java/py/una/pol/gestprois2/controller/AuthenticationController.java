df168edca0d135ddd2cd89628495ac04c7769633
dlopez <dlopezolazar93@gmail.com>
2018-03-17 16:48:46 -0300

    Servicio REST para Login

--- Projects/gestprois2-backend/src/main/java/py/una/pol/gestprois2/controller/AuthenticationController.java
+++ Projects/gestprois2-backend/src/main/java/py/una/pol/gestprois2/controller/AuthenticationController.java
@@ -5,50 +5,61 @@
  */
 package py.una.pol.gestprois2.controller;
 
+import java.util.Base64;
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.apache.log4j.Logger;
+import py.una.pol.gestprois2.entities.Usuario;
+import py.una.pol.gestprois2.facade.UsuarioFacade;
+import py.una.pol.gestprois2.request.Credentials;
+import py.una.pol.gestprois2.response.CredentialsOut;
 
 /**
  *
  * @author Diego
  */
 @Path("/authentication")
+@Stateless
 public class AuthenticationController {
+    
+    @EJB
+    private UsuarioFacade facadeUsu;
+    
+    private static final Logger LOGGER = Logger.getLogger(AuthenticationController.class);
+    
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response authenticateUser(@FormParam("username") String username, 
-                                     @FormParam("password") String password) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response authenticateUser(Credentials credentials) {
         try {
 
             // Authenticate the user using the credentials provided
-            authenticate(username, password);
-
-            // Issue a token for the user
-            String token = issueToken(username);
-
-            // Return the token on the response
-            return Response.ok(token).build();
-
+            Usuario u = facadeUsu.validateUser(credentials.getUsername(), credentials.getPassword());
+//          // Issue a token for the user
+            String token = issueToken(u.getNombreCompleto()+credentials.getPassword());
+//
+//          // Return the token on the response
+            CredentialsOut out = new CredentialsOut();
+            out.setAccesToken(token);
+            out.setSession(new Date());
+            
+            return Response.ok(out).build();
+            
         } catch (Exception e) {
+            LOGGER.error(e);
             return Response.status(Response.Status.FORBIDDEN).build();
         }      
     }
-    
-    private void authenticate(String username, String password) throws Exception {
-        // Authenticate against a database, LDAP, file or whatever
-        // Throw an Exception if the credentials are invalid
-    }
 
-    private String issueToken(String username) {
-        // Issue a token (can be a random String persisted to a database or a JWT token)
-        // The issued token must be associated to a user
-        // Return the issued token
-        return "";
+    private String issueToken(String param) {
+        
+        return Base64.getEncoder().encodeToString((param).getBytes());
     }
 }
