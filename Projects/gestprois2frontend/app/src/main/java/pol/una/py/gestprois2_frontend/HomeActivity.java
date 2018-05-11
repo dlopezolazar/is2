4cade29d4db79021ed38a027d9a68ed4c206d6be
dlopez <dlopezolazar93@gmail.com>
2018-05-11 18:03:13 -0400

    Integracion con servicio de backend

---
+++ Projects/gestprois2frontend/app/src/main/java/pol/una/py/gestprois2_frontend/HomeActivity.java
@@ -0,0 +1,13 @@
+package pol.una.py.gestprois2_frontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
